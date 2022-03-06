d66e08e3abff34f2ff3ebcd82f42b4bddaed367d
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-03-06 19:33:43 -0300

    feat(App): added new templates and paths

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,27 @@ import GlobalStyle from "styles/global";
 import { ThemeProvider } from "styled-components";
 import { Router, RouteComponentProps, Redirect } from "@reach/router";
 import { Wrapper } from "components";
-import Curriculum from "templates/Curriculum";
 
 function App() {
-  const Home = (props: RouteComponentProps) => <T.Home />;
-  const Curriculum = (props: RouteComponentProps) => <T.Curriculum />;
-  const Portfolio = (props: RouteComponentProps) => <T.Portfolio />;
+	const Home = (props: RouteComponentProps) => <T.Home />;
+	const Curriculum = (props: RouteComponentProps) => <T.Curriculum />;
+	const Works = (props: RouteComponentProps) => <T.Works />;
+	const Store = (props: RouteComponentProps) => <T.Store />;
 
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Wrapper>
-        <Router>
-          <Redirect from={routes.default} to={routes.home} />
-          <Home path={routes.home} />
-          <Curriculum path={routes.curriculum} />
-          <Portfolio path={routes.portfolio} />
-        </Router>
-      </Wrapper>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<Wrapper>
+				<Router>
+					<Redirect from={routes.default} to={routes.home} />
+					<Home path={routes.home} />
+					<Works path={routes.works} />
+					<Store path={routes.store} />
+					<Curriculum path={routes.curriculum} />
+				</Router>
+			</Wrapper>
+		</ThemeProvider>
+	);
 }
 
 export default App;

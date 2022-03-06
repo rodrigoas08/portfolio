9973161c44ad83eb879e221160ea12aea136d61a
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-03-06 18:57:04 -0300

    feat(button): increase rippling style button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,27 +2,30 @@ import * as S from "./styles";
 import { ButtonHTMLAttributes, memo } from "react";
 
 export type ButtonProps = {
-  secondary?: boolean;
-  fullWidth?: boolean;
+	secondary?: boolean;
+	fullWidth?: boolean;
+	rippling?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({
-  onClick,
-  children,
-  secondary,
-  fullWidth,
-  disabled = false,
-  ...props
+	onClick,
+	children,
+	secondary,
+	fullWidth,
+	rippling,
+	disabled = false,
+	...props
 }: ButtonProps) => (
-  <S.Wrapper
-    onClick={onClick}
-    disabled={disabled}
-    secondary={secondary}
-    fullWidth={fullWidth}
-    {...props}
-  >
-    {children}
-  </S.Wrapper>
+	<S.Wrapper
+		onClick={onClick}
+		disabled={disabled}
+		secondary={secondary}
+		fullWidth={fullWidth}
+		rippling={rippling}
+		{...props}
+	>
+		{children}
+	</S.Wrapper>
 );
 
 export default memo(Button);

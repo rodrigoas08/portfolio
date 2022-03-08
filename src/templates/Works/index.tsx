1eef6e5256a96736e5f4d9da4593c607e49ac7c2
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-03-08 00:23:50 -0300

    feat(works): created title and testing style css template

--- src/templates/Works/index.tsx
+++ src/templates/Works/index.tsx
@@ -1,27 +1,28 @@
-import * as S from "./styles";
+import * as S from './styles';
 
 export const Works = () => {
-	return (
-		<S.Content>
-			<S.Ul>
-				<S.Li>
-					Serviço de consultoria para montar seu computador incluindo orçamento
-					e escolha de peças.
-				</S.Li>
+  return (
+    <S.Content>
+        <S.Title>Principais serviços</S.Title>
+      <S.Ul>
+        <S.Li>
+          Serviço de consultoria para montar seu computador incluindo orçamento
+          e escolha de peças.
+        </S.Li>
 
-				<S.Li>
-					Montagem e Manutenção preventiva aumentando a vida útil do seu
-					computador (Limpeza completa e troca de pasta térmica).
-				</S.Li>
+        <S.Li>
+          Montagem e Manutenção preventiva aumentando a vida útil do seu
+          computador (Limpeza completa e troca de pasta térmica).
+        </S.Li>
 
-				<S.Li>Cable Management (Organização de cabos no gabinete)</S.Li>
+        <S.Li>Cable Management (Organização de cabos no gabinete)</S.Li>
 
-				<S.Li>Atendimento em domicílio ou remoto.</S.Li>
+        <S.Li>Atendimento em domicílio ou remoto.</S.Li>
 
-				<S.Li>Atendimento em domicílio ou remoto.</S.Li>
-			</S.Ul>
-		</S.Content>
-	);
+        <S.Li>Atendimento em domicílio ou remoto.</S.Li>
+      </S.Ul>
+    </S.Content>
+  );
 };
 
 export default Works;
--- src/templates/Works/styles.ts
+++ src/templates/Works/styles.ts
@@ -1,19 +1,31 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
-export const Content = styled.div`
-	width: 100%;
-	display: flex;
-	height: calc(100vh - 180px);
+export const Content = styled.section`
+  width: 100%;
+  display: grid;
+  justify-content: center;
+  grid-template-rows: 0.2fr 1fr;
+  height: calc(100vh - 180px);
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => css`
+    font-size: 4rem;
+    text-align: center;
+    letter-spacing: 0.5rem;
+    text-transform: uppercase;
+    text-shadow: -4px 10px 1px ${theme.colors.primary};
+  `}
 `;
 
 export const Ul = styled.ul`
-	width: 100%;
-	padding-top: 10rem;
-	font-size: 1.3rem;
-	list-style: inside square;
-	/* box-shadow: 0px 4px 4px 0px black; */
+  width: 100%;
+  padding-top: 10rem;
+  font-size: 2.3rem;
+  list-style: inside square;
+  border: 1px solid black;
 `;
 
 export const Li = styled.li`
-	padding: 2rem;
+  padding: 2rem;
 `;

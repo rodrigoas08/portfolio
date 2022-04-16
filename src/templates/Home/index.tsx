307b78209138170cd5cd3dd541d7c1197dcef92b
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-04-15 21:40:01 -0300

    style(home): change style and remove localization

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -2,35 +2,28 @@ import * as S from './styles';
 import routes from 'utils/routes';
 import { Button } from 'components';
 import { navigate } from '@reach/router';
-import Works from '../Works';
-import Localization from './Localization';
 
 const Home = () => {
   return (
-    <>
-      <S.Container>
-        <S.Section>
-          <S.Title>
-            Rodrigo
-            <br /> A. Sobral
-          </S.Title>
-          <S.Subtitle>Técnico em informática e desenvolvedor web</S.Subtitle>
-        </S.Section>
+    <S.Container>
+      <S.Section>
+        <S.Title>Rodrigo Sobral</S.Title>
+        <S.Subtitle>
+          Técnico em informática e desenvolvedor front-end
+        </S.Subtitle>
+      </S.Section>
 
-        <S.Article>
-          <S.Paragraph>
-            Seu <span>computador ficou lento</span> e não sabe{' '}
-            <span> como resolver</span>?
-          </S.Paragraph>
-          <S.Seta src={`${process.env.PUBLIC_URL}/img/seta.svg`} />
-          <Button rippling onClick={() => navigate(routes.works)}>
-            Conheça meus serviços
-          </Button>
-        </S.Article>
-      </S.Container>
-      <Localization />
-      <Works />
-    </>
+      <S.Article>
+        <S.Paragraph>
+          Seu <span>computador ficou lento</span> e não sabe{' '}
+          <span> como resolver</span> ?
+        </S.Paragraph>
+        <S.Seta src={process.env.PUBLIC_URL + '/img/seta.svg'} />
+        <Button rippling onClick={() => navigate(routes.works)}>
+          Conheça meus serviços
+        </Button>
+      </S.Article>
+    </S.Container>
   );
 };
 
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,49 +1,69 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  display: grid;
-  height: calc(100vh - 6rem);
-  grid-template-columns: 1fr 0.8fr;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  height: calc(100vh - 16rem);
 `;
 
 export const Section = styled.section`
-  ${({ theme }) => css`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-    background-color: ${theme.colors.primary};
-  `}
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: transparent;
 `;
 
 export const Title = styled.h1`
   ${({ theme }) => css`
-    font-size: 7rem;
+    font-size: 5rem;
+    cursor: default;
     position: relative;
-    letter-spacing: 0.3rem;
-    text-transform: uppercase;
-    color: ${theme.colors.text};
-    font-weight: ${theme.font.bold};
-    text-shadow: 0px 2px 1px ${theme.colors.white};
+    color: ${theme.colors.white};
+    font-weight: ${theme.font.light};
+    font-family: ${theme.font.family.montserrat};
+    /* text-shadow: 0px 2px 1px ${theme.colors.white}; */
 
-    ::after {
-      content: '';
-      width: 100%;
-      height: 0.2rem;
-      display: block;
-      position: absolute;
-      background-color: white;
-      box-shadow: 5px 3px 1px rgba(0, 0, 0, 0.6);
+    &:hover {
+      :after {
+        content: '';
+        width: 100%;
+        opacity: 0.5;
+        display: block;
+        height: 0.1rem;
+        position: absolute;
+        background-color: ${theme.colors.primary};
+        box-shadow: 5px 3px 1px rgba(0, 0, 0, 0.6);
+      }
+
+      ::before {
+        content: '';
+        top: 1.1rem;
+        opacity: 0.4;
+        left: 12.45rem;
+        display: block;
+        width: 0.57rem;
+        height: 0.57rem;
+        border-radius: 50%;
+        position: absolute;
+        background-color: ${theme.colors.primary};
+      }
     }
   `}
 `;
 
 export const Subtitle = styled.h4`
-  padding-top: 0.6rem;
-  padding-bottom: 5rem;
-  text-transform: uppercase;
+  ${({ theme }) => css`
+    cursor: default;
+    padding-top: 0.6rem;
+    padding-bottom: 5rem;
+    text-transform: uppercase;
+    color: ${theme.colors.primary};
+  `}
 `;
 
 export const Article = styled.article`
@@ -54,26 +74,17 @@ export const Article = styled.article`
   align-items: center;
   flex-direction: column;
   justify-content: center;
-  background-color: ${({ theme }) => theme.colors.text};
-  > button {
-    width: 25rem;
-    height: 8rem;
-    text-transform: uppercase;
-    font-weight: ${({ theme }) => theme.font.extraBold};
-    ::before {
-      border: 1.2rem solid ${({ theme }) => theme.colors.white};
-    }
-  }
+  background-color: transparent;
 `;
 
 export const Seta = styled.img`
-  height: 6rem;
-  width: 6rem;
+  height: 2rem;
+  width: 2rem;
   animation: is-bouncing 1.75s ease-in alternate infinite;
 
   @keyframes is-bouncing {
     to {
-      transform: translateY(40px);
+      transform: translateY(30px);
     }
   }
 `;
@@ -81,9 +92,8 @@ export const Seta = styled.img`
 export const Paragraph = styled.p`
   ${({ theme }) => css`
     color: ${theme.colors.white};
-    font-weight: ${theme.font.bold};
+    font-weight: ${theme.font.light};
     font-size: ${theme.font.sizes.xxlarge};
-    text-shadow: -1px 1px 1px ${theme.colors.black};
     > span {
       color: ${theme.colors.primary};
     }

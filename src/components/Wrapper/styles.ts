4bc512469d593d23fbbaf6b66411ee4d3c96984f
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-04-15 21:38:36 -0300

    feat(home): add background animated(particlesbg)

--- src/components/Wrapper/styles.ts
+++ src/components/Wrapper/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.main`
-  display: flex;
-  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
 `;

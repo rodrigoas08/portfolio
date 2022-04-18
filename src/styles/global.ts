89d38f9e996b7c7b913cfe3c30e88463a9c862ad
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-04-18 00:59:00 -0300

    styles(scrollbar): add style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,12 +17,27 @@ const GlobalStyle = createGlobalStyle`
       theme.colors.primary}, ${({ theme }) => theme.colors.blueRibbon}); 
     background: -webkit-linear-gradient(180deg, ${({ theme }) =>
       theme.colors.primary}, ${({ theme }) => theme.colors.blueRibbon}); */
+      
+    ::-webkit-scrollbar {
+      width: 0.3rem;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: ${({ theme }) => theme.colors.black};
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      border-radius: 0.1rem;
+      background: ${({ theme }) => theme.colors.primary};
+    }
 
   }
   
   html {
     font-size: 62.5%;
-  } 
+  }
 `;
 
 export default GlobalStyle;

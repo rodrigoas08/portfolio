bfa5dee4080950e09ddb9ba082a5e18901e09a85
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-04-21 23:15:52 -0300

    fix(home): change link to aboutme

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -24,7 +24,7 @@ const Home = () => {
         </S.Section>
         <Link
           activeClass="active"
-          to="servicos"
+          to="aboutme"
           spy={true}
           smooth={true}
           offset={0}

0e7566522c835454f44a158fa72fd9650e409e1d
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-04-22 18:10:39 -0300

    Style(about): add opacity on background

--- src/templates/AboutMe/styles.ts
+++ src/templates/AboutMe/styles.ts
@@ -12,6 +12,7 @@ export const Wrapper = styled.div.attrs({ id: 'aboutme' })`
 export const Container = styled.div`
   ${({ theme }) => css`
     width: 100%;
+    opacity: 0.9;
     display: flex;
     flex-wrap: wrap;
     background: #b4b4b4;
@@ -48,22 +49,23 @@ export const Section = styled.section`
   ${({ theme }) => css`
     width: 100%;
     height: auto;
-    padding: 4rem 25rem;
     backdrop-filter: blur(1.5rem);
     background-color: rgba(0, 0, 0 0.1);
+    padding: ${theme.spacings.large} 25rem;
   `}
 `;
 
 export const Image = styled.div`
-  float: left;
-  width: 15rem;
-  height: 15rem;
-  margin-right: 2rem;
-  border-radius: 50%;
-  background-clip: content-box;
-  background-size: 100% 100%;
-  background-repeat: no-repeat;
-  background-image: url(/img/euPedro.jpg);
+  ${({ theme }) => css`
+    float: left;
+    width: 15rem;
+    height: 15rem;
+    border-radius: 50%;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    margin-right: ${theme.spacings.small};
+    background-image: url(/img/euPedro.jpg);
+  `}
 `;
 
 export const Paragraph = styled.p`

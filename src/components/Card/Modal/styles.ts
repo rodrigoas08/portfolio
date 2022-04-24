cacaf161ad5602b0cd833a62d5c030e94a23d455
DESKTOP-5Q63D0A\Sobral <rodrigo.sobral@usabit.com.br>
2022-04-24 20:28:09 -0300

    styles(card): add loremipsum

--- src/components/Card/Modal/styles.ts
+++ src/components/Card/Modal/styles.ts
@@ -1,17 +1,19 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const BoxShadow = styled.div`
   inset: 0;
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   z-index: 2;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
   background-color: rgba(0, 0, 0, 0.4);
 `;
 
 export const BoxModal = styled.div`
   width: 40rem;
   height: 40rem;
-  background-color: red;
+  background-color: white;
+  box-sizing: border-box;
+  overflow: scroll;
 `;

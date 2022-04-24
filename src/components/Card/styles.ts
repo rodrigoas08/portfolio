d91d41eae01db686c44c127149909e919b95055d
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-04-24 20:44:27 -0300

    fix(cards): adjust position of the cards and remove unused comments

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -4,36 +4,32 @@ import * as ButtonStyles from 'components/Button/styles';
 export const Card = styled.div`
   ${({ theme }) => css`
     width: 25%;
+    gap: 1rem;
     display: flex;
-    flex-wrap: wrap;
     min-height: 15rem;
     break-inside: auto;
     align-items: center;
-    word-wrap: break-word;
-    justify-content: center;
+    flex-direction: column;
     color: ${theme.colors.white};
     backdrop-filter: blur(1.5rem);
+    justify-content: space-between;
     padding: ${theme.spacings.small};
     font-size: ${theme.font.sizes.small};
     background-color: rgba(255, 255, 255, 0.1);
     border-top: 0.1rem solid ${theme.colors.primary};
 
-    /* :nth-child(2n) {
-    align-self: end;
-    justify-content: start;
-  } */
-
     ${ButtonStyles.Wrapper} {
       width: 10rem;
-      font-size: 1rem;
       letter-spacing: 0.1rem;
-      align-self: flex-end;
-      color: white;
+      font-size: ${theme.font.sizes.xxsmall};
+
+      :hover{
+        opacity: 0.8;
+      }
     }
   `}
 `;
 
 export const Cardtext = styled.p`
-  color: white;
   text-align: center;
 `;

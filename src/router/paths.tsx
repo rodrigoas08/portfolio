ab14c4027a31a4cb982487022d4f06c297386197
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-06-25 12:41:03 -0300

    refactor(router): rename browserRouter as Router

--- src/router/paths.tsx
+++ src/router/paths.tsx
@@ -4,13 +4,18 @@ import theme from 'styles/theme';
 import GlobalStyle from 'styles/global';
 import Wrapper from 'components/Wrapper';
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes
+} from 'react-router-dom';
 
 const RouterView = () => {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <GlobalStyle />
+      <GlobalStyle />
+      <Router>
         <Wrapper>
           <Routes>
             <Route
@@ -20,7 +25,7 @@ const RouterView = () => {
             <Route path={routes.home} element={<T.Home />} />
           </Routes>
         </Wrapper>
-      </BrowserRouter>
+      </Router>
     </ThemeProvider>
   );
 };

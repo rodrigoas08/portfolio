bd57321fd0edd1805c6bc3c110dcb6690a8da065
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-06-27 01:07:54 -0300

    styles(icons): apply theme and remove comented lines

--- src/components/Icons/styles.ts
+++ src/components/Icons/styles.ts
@@ -17,16 +17,10 @@ export const Wrapper = styled.div<IconProps>`
       cursor: pointer;
       flex-direction: row;
       padding: 0 ${theme.spacings.small};
-      /* background-image: linear-gradient(
-        to bottom,
-        ${theme.colors.primary} 0.19%,
-        transparent 29.45%
-      ); */
-
       border-radius: ${theme.border.radius};
+      font-size: ${theme.font.sizes.medium};
       background-color: rgba(255, 255, 255, 0.1);
       border-top: 0.1rem solid ${theme.colors.primary};
-      font-size: ${theme.font.sizes.medium};
     `}
 
     > p {

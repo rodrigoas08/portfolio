b6677c1708dffd235d71e0a2bc53d913903b0730
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-11-12 02:07:12 -0300

    refactor(header): refactoring code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import { useState, memo, useLayoutEffect } from 'react';
 import { IColorProps, ITitleName } from 'interfaces/header';
 
 export const Header = () => {
-  const [title, setTitle] = useState<ITitleName>({ name: 'Início' });
+  const [title, setTitle] = useState<ITitleName[]>([{ name: 'Início' }]);
   const [colorHeader, setColorHeader] = useState<IColorProps>({
     color: 'transparent'
   });
@@ -21,90 +21,31 @@ export const Header = () => {
         <S.ImgProfile />
       </Link>
       <S.NavWrapper>
-        <li>
-          <Link
-            to="inicio"
-            spy={true}
-            smooth={true}
-            onSetActive={() => {
-              setTitle({ name: 'Início' });
-              changeTitleOfPage({ name: 'Início' });
-            }}
-          >
-            <S.LinkMenu
-              activeLink={title.name === 'Início'}
-              onClick={() => {
-                setTitle({ name: 'Início' });
-                changeTitleOfPage({ name: 'Início' });
-              }}
-            >
-              Início
-            </S.LinkMenu>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="sobre"
-            spy={true}
-            smooth={true}
-            onSetActive={() => {
-              setTitle({ name: 'Sobre' });
-              changeTitleOfPage({ name: 'Sobre' });
-            }}
-          >
-            <S.LinkMenu
-              activeLink={title.name === 'Sobre'}
-              onClick={() => {
-                setTitle({ name: 'Sobre' });
-                changeTitleOfPage({ name: 'Sobre' });
-              }}
-            >
-              Sobre
-            </S.LinkMenu>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="servicos"
-            spy={true}
-            smooth={true}
-            onSetActive={() => {
-              setTitle({ name: 'Serviços' });
-              changeTitleOfPage({ name: 'Serviços' });
-            }}
-          >
-            <S.LinkMenu
-              activeLink={title.name === 'Serviços'}
-              onClick={() => {
-                setTitle({ name: 'Serviços' });
-                changeTitleOfPage({ name: 'Serviços' });
-              }}
-            >
-              Serviços
-            </S.LinkMenu>
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="contato"
-            spy={true}
-            smooth={true}
-            onSetActive={() => {
-              setTitle({ name: 'Contato' });
-              changeTitleOfPage({ name: 'Contato' });
-            }}
-          >
-            <S.LinkMenu
-              activeLink={title.name === 'Contato'}
-              onClick={() => {
-                setTitle({ name: 'Contato' });
-                changeTitleOfPage({ name: 'Contato' });
-              }}
-            >
-              Contato
-            </S.LinkMenu>
-          </Link>
-        </li>
+        {title.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link
+                to={link.name}
+                spy={true}
+                smooth={true}
+                onSetActive={() => {
+                  setTitle([{ name: link.name }]);
+                  changeTitleOfPage({ name: link.name });
+                }}
+              >
+                <S.LinkMenu
+                  activeLink={link.name === 'Início'}
+                  onClick={() => {
+                    setTitle([{ name: link.name }]);
+                    changeTitleOfPage({ name: 'Início' });
+                  }}
+                >
+                  {link.name}
+                </S.LinkMenu>
+              </Link>
+            </li>
+          );
+        })}
       </S.NavWrapper>
     </S.Wrapper>
   );

1658b1059f4bf5646d7dddcf69ccd02b5db71c77
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2022-12-25 17:00:04 -0300

    styles(card/portfolio): tweaked value of transitions in the card component

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,5 +1,5 @@
-import styled, { css } from 'styled-components';
 import { CardProps } from '.';
+import styled, { css } from 'styled-components';
 
 export const Card = styled.div`
   ${({ theme }) => css`
@@ -9,17 +9,15 @@ export const Card = styled.div`
     min-height: 30rem;
     backdrop-filter: blur(1.5rem);
     grid-template-rows: 1fr 0.8fr;
-    transition: grid-template-rows 0.8s ease-in-out;
+    transition: all 0.8s ease-in-out;
     border-radius: ${theme.border.radius};
     background-color: rgba(255, 255, 255, 0.1);
     border-top: 0.3rem solid ${theme.colors.primary};
 
     :hover {
       grid-template-rows: 0.2fr 1fr;
-      transition: grid-template-rows 0.8s ease-in-out;
 
       ${CardImage} {
-        transition: 0.8s;
         opacity: 0.2;
       }
 
@@ -40,8 +38,8 @@ export const Card = styled.div`
 
 export const CardImage = styled.div<CardProps>`
   ${({ background }) => css`
-    transition: 1.8s ease-in-out;
     background-size: cover;
+    transition: 0.8s ease-in-out;
     background-image: url(${background});
   `}
 `;
@@ -53,9 +51,9 @@ export const CardInfo = styled.div`
     align-items: center;
     flex-direction: column;
     justify-content: center;
+    transition: 0.8s ease-in-out;
     gap: ${theme.spacings.xxsmall};
     border-radius: 0 0 0.3rem 0.3rem;
-    transition: all 0.4s ease-in-out;
     padding: ${theme.spacings.xxsmall};
     background: ${theme.colors.primary};
     box-shadow: 0.1rem 0rem 1.5rem rgba(0, 0, 0, 1);

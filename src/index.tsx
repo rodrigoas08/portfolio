e92ad1ae5e98e03c237b11a1d31c7486b6fe082d
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2023-01-11 13:55:00 -0300

    chore: install and configure vitejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));

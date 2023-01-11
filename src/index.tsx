e982905dab22b1f010f49c5c1814f13dfd8b085c
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2023-01-11 14:27:00 -0300

    chore: return with reportwebVitals to measure performance

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
-import { render } from 'react-dom';
 import App from './App';
+import { render } from 'react-dom';
+import reportWebVitals from 'reportWebVitals';
 
 render(<App />, document.getElementById('root'));
+reportWebVitals();

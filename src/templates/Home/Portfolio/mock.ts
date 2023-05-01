5a02ddb0a76b07f22049e625f36625cd6a819326
Rodrigo Sobral <sobral_rodrigo@yahoo.com.br>
2023-05-01 20:35:37 -0300

    styles: changed card layout

--- src/templates/Home/Portfolio/mock.ts
+++ src/templates/Home/Portfolio/mock.ts
@@ -2,21 +2,28 @@ import Ecbporto from 'img/portfolios/ecbporto.webp';
 
 export const CardList = [
   {
-    projectName: 'ecbporto',
+    title: 'ecbporto',
     background: `${Ecbporto}`,
-    link: 'http://www.ecbporto.com',
-    repository: 'teste'
+    description:
+      'É uma Landing Page criada para a empresa Ecbporto que ofereçe diversos tipos de soluções para realizações de projetos...',
+    stack: ['React', 'Typescript', 'Styled Components'].join(' + '),
+    repository: 'teste',
+    link: 'http://www.ecbporto.com'
   },
   {
-    projectName: '',
+    title: '',
     background: '',
-    link: '',
-    repository: ''
+    descriptiopn: '',
+    stack: '',
+    repository: '',
+    link: ''
   },
   {
-    projectName: '',
+    title: '',
     background: '',
-    link: '',
-    repository: ''
+    descriptiopn: '',
+    stack: '',
+    repository: '',
+    link: ''
   }
 ];

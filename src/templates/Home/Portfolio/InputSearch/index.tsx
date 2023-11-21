e191c8dd60bbacec1bc238771c9b3648cf6afd33
Rodrigo Sobral <rodrigo.sobral@animaeducacao.com.br>
2023-11-21 13:38:39 -0300

    style: change the css inputsearch

--- src/templates/Home/Portfolio/InputSearch/index.tsx
+++ src/templates/Home/Portfolio/InputSearch/index.tsx
@@ -10,13 +10,16 @@ interface IInputSearchProps {
 const InputSearch = ({ search, setSearch }: IInputSearchProps) => {
   return (
     <Wrapper>
+      <Icon id="icon">
+        <IconSearch size={15} title="Ícone de busca" />
+      </Icon>
       <SearchInput
+        fullWidth
         type="text"
         value={search}
         placeholder="Buscar"
         onChange={(event) => setSearch(event.target.value)}
       />
-      <IconSearch size={15} title="Ícone de busca" />
     </Wrapper>
   );
 };
@@ -24,23 +27,37 @@ export default InputSearch;
 
 const Wrapper = styled.div`
   width: 30rem;
+  position: relative;
   margin-top: ${({ theme }) => theme.spacings.small};
+
+  :focus-within #icon {
+    animation: bg 1s;
+    filter: brightness(50%);
+    transition: all 1s linear;
+
+    @keyframes bg {
+      from {
+        transform: rotate(0deg);
+        background-color: transparent;
+      }
+      to {
+        transform: rotate(360deg);
+        background-color: transparent;
+      }
+    }
+  }
 `;
 
 const SearchInput = styled(Input)`
   ${({ theme }) => css`
-    width: 100%;
-    border-radius: 0;
     text-transform: none;
+    border-radius: 0.1rem;
     text-overflow: ellipsis;
-    transition: all 1.5s ease-in-out;
-    background: ${theme.colors.grayIce};
     font-size: ${theme.font.sizes.medium};
-    padding-left: ${theme.spacings.xxsmall};
+    padding-left: ${theme.spacings.large};
     padding-right: ${theme.spacings.medium};
 
     :focus {
-      background: transparent;
       border: 0.1rem solid transparent;
       transition: all 1.5s ease-in-out;
       border-bottom: 0.1rem solid ${theme.colors.primary};
@@ -52,9 +69,21 @@ const SearchInput = styled(Input)`
   `}
 `;
 
+export const Icon = styled.div`
+  ${({ theme }) => css`
+    width: 3.5rem;
+    height: 100%;
+    display: flex;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    transition: 1s ease-in-out;
+    background-color: ${theme.colors.grayIce};
+  `}
+`;
+
 const IconSearch = styled(SearchAlt)`
   position: absolute;
   fill: url('#gradient');
-  transform: translate(-150%, 70%);
   filter: drop-shadow(0 0.3rem 0.2rem ${({ theme }) => theme.colors.black});
 `;

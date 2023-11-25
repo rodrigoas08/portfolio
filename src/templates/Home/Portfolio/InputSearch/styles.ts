c8a075c40f40c828a9e28569ae7b261623efc452
Rodrigo Sobral <rodrigo.sobral@animaeducacao.com.br>
2023-11-25 19:15:45 -0300

    removed unused import

--- src/templates/Home/Portfolio/InputSearch/styles.ts
+++ src/templates/Home/Portfolio/InputSearch/styles.ts
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components';
 import { Input } from 'components/Input/styles';
-import { FaSearch } from 'react-icons/fa';
 import { BiSearchAlt } from 'react-icons/bi';
 
 export const Wrapper = styled.div`
